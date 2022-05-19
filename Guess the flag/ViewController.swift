2de2839678f1c804f93dbb61702767e51bcfe676
Iuliia Bondarenko <bondarienko-iuliia@mail.ru>
2022-05-19 13:22:10 +0300

    final score

--- Guess the flag/ViewController.swift
+++ Guess the flag/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var questionsAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,26 +34,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     func askQuestion(action:UIAlertAction! = nil){
+        questionsAsked += 1
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-        title = countries[correctAnswer].uppercased()
+        title = countries[correctAnswer].uppercased() + " score: \(score)"
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        var message = ""
         if sender.tag == correctAnswer{
             title = "Correct"
             score += 1
         }
         else{
             title = "Wrong"
+            message = "That’s the flag of \(countries[correctAnswer].uppercased())"
             score -= 1
         }
-        
-        let ac = UIAlertController(title: title, message: "You score is \(score)", preferredStyle: .alert)
+        if(questionsAsked > 9){
+            message = "Your final score is: \(score)"
+            score = 0
+            questionsAsked = 0
+        }
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
         present(ac, animated: true)
     }
